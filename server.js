f6d4dc2a96ccfb29ef94d42958d45a5473f383ec
Akhilpr8055 <akhilpr8055@gmail.com>
2016-09-26 20:02:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-wap article-one = {
+var articleOne = {
     
-    title:'article-one',
-    heading:'surya frends',
-    body: `this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows paddingthis is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows paddingthis is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding`
+    title :'article-one',
+    heading :'surya frends',
+    content :`this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows paddingthis is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows paddingthis is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding this is a demo shows padding`
     };
 html template = { 
     
