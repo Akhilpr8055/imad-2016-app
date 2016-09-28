9e1a9add10656f54341525b8adf61962c9d2022c
Akhilpr8055 <akhilpr8055@gmail.com>
2016-09-28 22:53:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,12 +77,15 @@ return httpTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleNmae',function(req,res){
-    var articleName=req.params.articleNmae;
-   res.send(createTemplate(articles[articleName]));
-   
+app.get('/article-one',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-
+app.get('/article-two',function(req,res){
+       res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+app.get('/article-three',function(req,res){
+      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
