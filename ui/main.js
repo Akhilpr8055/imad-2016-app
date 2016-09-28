894cd25e9d8d3a5e89e29274571614b96ad4d4cc
Akhilpr8055 <akhilpr8055@gmail.com>
2016-09-29 00:22:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,6 @@
-console.log('Loaded!');
-var element = document.getElementById('name1');
-
-element.innerHTML='AKHIL';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function()
-{
-    var intervel = setInterval(moveRight, 50);
+var button = element.getElementById('counter');
+button.onclick = function(){
+counter = counter = 1;
+var span = element.getElementById('count');
+span.innerHTML = counter.toString();
 };
