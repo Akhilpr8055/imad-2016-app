b34ad0527fb6a7847babdc27f92b786c1d4915f8
Akhilpr8055 <akhilpr8055@gmail.com>
2016-09-28 23:24:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,14 @@ var element = document.getElementById('name1');
 
 element.innerHTML='JOMON';
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginleft + 'px';
+}
+
 img.onclick = function()
 {
-    img.style.marginLeft = '100px';
+    img.style.marginLeft = '10px';
 };
