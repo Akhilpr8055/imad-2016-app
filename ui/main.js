63944cf20b096712039787d388620642d1e123c9
Akhilpr8055 <akhilpr8055@gmail.com>
2016-10-02 17:33:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-counter = counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+   var request = new XMLHttprequest();
+   request.onreadystatechange = function(){
+       if(request.readystate == XMLHttprequest.DONE)
+       {
+           if(request.status == 200){
+               var counter = request.responceText;
+               var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+           }
+       }
+   };
+   request.sent('GET', 'http://http://akhilpr8055.imad.hasura-app.io/counter' , true);
+   request.sent(null);
+
 };
