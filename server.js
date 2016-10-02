90b087392dae707515d0b0d3cfc35958cafab987
Akhilpr8055 <akhilpr8055@gmail.com>
2016-10-02 21:04:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,13 +84,6 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
-var names=[];
-app.get('/submit-name/:name', function(req, res){
-   var name = req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-   
-
 
 app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
@@ -113,7 +106,12 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names=[];
+app.get('/submit-name/:name', function(req, res){
+   var name = req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+   
    
    
     
